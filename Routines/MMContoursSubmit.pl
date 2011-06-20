f1d77de72b7e9b4a009201b15556cc06fcb7d01f
boyle <boyle@aeca8a61-ec13-0410-a8d4-8d6e48c7bcfb>
2011-06-20 17:59:11 +0000

    Adding a simple resubmission script for MMContours
    git-svn-id: svn+ssh://sxs-archive.tapir.caltech.edu/SpEC/SVNroot/trunk/Support/WaveExtrapolation/Waveforms@17147 aeca8a61-ec13-0410-a8d4-8d6e48c7bcfb

---
+++ Routines/MMContoursSubmit.pl
@@ -0,0 +1,35 @@
+#! /usr/bin/perl
+
+$q = $ARGV[0];
+$chis = $ARGV[1];
+$NSegments = $ARGV[2];
+$Command = "MMContours";
+
+# Divide up into segments
+use POSIX;
+$FreqsPerSegment = 20; # By hand
+# $FreqsPerSegment = (121.0/($NSegments-0.0)); # Inefficent but obvious
+# $FreqsPerSegment = ceil(121.0/($NSegments-0.0)); # Efficient but weird file ranges
+
+for $seg (1..$NSegments) {
+    # Determine the original frequency range
+    $OrigStartFreq = ceil((${seg}-1)*${FreqsPerSegment});
+    $OrigEndFreq = ceil(${seg}*${FreqsPerSegment});
+#     print "${seg} ${OrigStartFreq} ${OrigEndFreq}\t";
+    
+    # Look for this segment's FinishedStep
+    $FinishedStep = "FinishedStep_q${q}_chis${chis}_o${OrigStartFreq}-${OrigEndFreq}_m0";
+    if ( -e ${FinishedStep} ) {
+	my($StartFreq, $StartMass) = split(' ', `cat ${FinishedStep}`);
+	# If it is '0 0', this segment is done; skip to the next one
+	if($StartFreq==0 && $StartMass==0) { next; }
+	# If it is not '0 0', those are the new starting conditions
+    } else {
+	# If it does not exist, start with '0 ${OrigStarFreq}'
+	$StartMass = 0;
+	$StartFreq = $OrigStartFreq;
+    }
+    # Do the qsub
+    
+    print "echo \"${Command} $q $chis $StartMass $StartFreq $OrigEndFreq\" | qsub -l nodes=1,walltime=11:59:00 -d /home/boyle/${Command} -e q${q}_chis${chis}_${OrigStartFreq}-${OrigEndFreq}.err -o q${q}_chis${chis}_${OrigStartFreq}-${OrigEndFreq}.out -\n";
+}
