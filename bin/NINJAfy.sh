3a96c0acb98276f36246160a307446f8cc413ea2
boyle <boyle@aeca8a61-ec13-0410-a8d4-8d6e48c7bcfb>
2011-08-16 16:32:04 +0000

    Getting the NINJAfy script working nicely
    git-svn-id: svn+ssh://sxs-archive.tapir.caltech.edu/SpEC/SVNroot/trunk/Support/WaveExtrapolation/Waveforms@17914 aeca8a61-ec13-0410-a8d4-8d6e48c7bcfb

---
+++ bin/NINJAfy.sh
@@ -0,0 +1,60 @@
+#! /bin/bash
+
+##################################
+## ADJUST THE FOLLOWING VARIABLES:
+##################################
+# Path to SpEC/Support/WaveExtrapolation/Waveforms/bin/:
+Waveforms=~/Waveforms/bin
+
+# Where to put the NINJA-format data:
+KrakenRepos=../../KrakenRepos/Case1
+
+# Relative path name of relevant bbh file:
+BBHFile=SpEC_Case1.bbh
+
+# Subdirectories (relative to pwd), each holding a copy of the data files:
+Levs=( "GW-Lev1.1.1" "GW-Lev2.2.2" "GW-Lev3.3.3" )
+
+# Files with names of "rh_R${radius}m.dat" will be extracted in each subdirectory of $Levs
+Radii=( "0400" "0100" )
+
+# Files with names of "rhOverM_ExtrapolatedN${radius}.dat" will be extracted in each subdirectory of $Levs
+ExtrapN=( "2" "3" "4" "5" )
+
+
+
+#########################################
+## (PROBABLY) DON'T ADJUST THE FOLLOWING:
+#########################################
+for lev in "${Levs[@]}"
+do
+    # Loop over finite-radius waveforms
+    for radius in "${Radii[@]}"
+    do
+	# Create the subdirectory in $KrakenRepos
+	subdir=${lev}/R${radius}m
+	mkdir -p ${KrakenRepos}/${subdir};
+	
+	# Copy the metadata file and adjust what needs adjusting
+	cp ${BBHFile} ${KrakenRepos}/${subdir}/${BBHFile};
+	perl -p -i -e "s/extraction-radius = finite-radii/extraction-radius = ${radius} # Units of total Christodoulou mass/" ${KrakenRepos}/${subdir}/${BBHFile}
+	
+	# Run the routine to convert to NINJA format and append the metadata file
+	${Waveforms}/OutputToNINJA ${lev}/rh_R${radius}m.dat ${KrakenRepos}/${subdir}/${BBHFile} ReIm;
+    done
+    
+    # Loop over extrapolated waveforms
+    for N in "${ExtrapN[@]}"
+    do
+	# Create the subdirectory in $KrakenRepos
+	subdir=${lev}/ExtrapolatedN${N}
+	mkdir -p ${KrakenRepos}/${subdir}
+	
+	# Copy the metadata file and adjust what needs adjusting
+	cp ${BBHFile} ${KrakenRepos}/${subdir}/${BBHFile}
+	perl -p -i -e "s/extraction-radius = finite-radii/extraction-radius = extrapolated # N=${N}/" ${KrakenRepos}/${subdir}/${BBHFile}
+	
+	# Run the routine to convert to NINJA format and append the metadata file
+	${Waveforms}/OutputToNINJA ${lev}/rhOverM_ExtrapolatedN${N}.dat ${KrakenRepos}/${subdir}/${BBHFile} MagArg
+    done
+done
