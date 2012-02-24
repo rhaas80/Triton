e96ab64f49d3a363b9b0b47c559aba5861b2bd9f
Mike Boyle <boyle@astro.cornell.edu>
2012-02-24 16:16:56 -0500

    Got Waveform.plot working nicely, and adding labels to the lines (for possible legends)

--- Python/PyGW/plot/__init__.py
+++ Python/PyGW/plot/__init__.py
@@ -8,7 +8,7 @@ from matplotlib.pyplot import plot as matplotlibpyplotplot
 from matplotlib.pyplot import semilogx as matplotlibpyplotsemilogx
 from matplotlib.pyplot import semilogy as matplotlibpyplotsemilogy
 from matplotlib.pyplot import loglog as matplotlibpyplotloglog
-from matplotlib.pyplot import xlabel, ylabel, tight_layout
+from matplotlib.pyplot import xlabel, ylabel, tight_layout, setp
 from warnings import warn
 from numpy import array, empty, transpose
 
@@ -44,39 +44,12 @@ def plot(this, WaveformPart='Mag', Modes=(), *pyplot_args, **pyplot_kwargs) :
         "h": r'h'
         }
     
-    XLabel=r'$(t-r_\ast)/M$'
-    YLabel=''
+    XLabel = r'$(t-r_\ast)/M$'
+    YLabel = ''
     
-    # if (WaveformPart.lower()=='mag') :
-    #     YLabel =r'$\mathrm{Mag} \left( ' + TypeDict[this.Type()] + r' \right) $' # r'$\mathrm{Mag}\left\{ a \right\}$'
-    #     if (type(Modes)==int or len(Modes)==1) :
-    #         Lines = matplotlibpyplotplot(this.T(), this.Mag(Modes), *pyplot_args, **pyplot_kwargs)
-    #     elif (len(Modes)==0) :
-    #         Lines = matplotlibpyplotplot(this.T(), this.Mag().transpose(), *pyplot_args, **pyplot_kwargs)
-    #     elif ((len(Modes)==2) and ((Modes[0]<2) or (Modes[0]<abs(Modes[1])))) :
-    #         Data = (this.Mag(Modes[0]), this.Mag(Modes[1]))
-    #         Data = transpose(Data)
-    #         Lines = matplotlibpyplotplot(this.T(), Data, *pyplot_args, **pyplot_kwargs)
-    #         #Lines = matplotlibpyplotplot(this.T(), transpose((this.Mag(Modes[0]),this.Mag(Modes[1]))), *pyplot_args, **pyplot_kwargs)
-    #     else :
-    #         Modes = array(Modes)
-    #         if (Modes.shape==(2,)) :
-    #             warn("Ambiguous 'Modes' specification.  Assuming this is an (l,m) specification.", SyntaxWarning)
-    #             #try: # Catch a bad (l,m) error
-    #             Lines = matplotlibpyplotplot(this.T(), this.Mag(this.FindModeIndex(int(Modes[0]), int(Modes[1]))), *pyplot_args, **pyplot_kwargs)
-    #         else :
-    #             Data = empty([Modes.shape[0], this.NTimes()], dtype=float)
-    #             for i in range(Modes.shape[0]) :
-    #                 if (type(Modes[i])==int) :
-    #                     ModeIndex = Modes[i]
-    #                 else :
-    #                     ModeIndex = this.FindModeIndex(int(Modes[i][0]), int(Modes[i][1]))
-    #                 Data[i] = this.Mag(ModeIndex)
-    #             Lines = matplotlibpyplotplot(this.T(), Data.transpose(), *pyplot_args, **pyplot_kwargs)
-    # else :
-    #     print("Unsupported data type %s" % WaveformPart)
-    #     return []
+    Labels = []
     
+    ## This decides which quantity to plot, what type of plot, and what the labels should be
     if (WaveformPart.lower()=='mag') :
         YLabel =r'$\mathrm{Mag} \left( ' + TypeDict[this.Type()] + r' \right) $'
         styledplot = matplotlibpyplotplot
@@ -97,25 +70,32 @@ def plot(this, WaveformPart='Mag', Modes=(), *pyplot_args, **pyplot_kwargs) :
         print("Unsupported data type %s" % WaveformPart)
         return []
     
+    ## This does the actual work of plotting, depending on what Modes are needed
     if (type(Modes)==int or len(Modes)==1) :
         Lines = styledplot(this.T(), quantity(Modes), *pyplot_args, **pyplot_kwargs)
+        Labels = ['(' + str(this.L(Modes)) + ', ' + str(this.M(Modes)) + ')']
     elif (len(Modes)==0) :
         Lines = styledplot(this.T(), quantity().transpose(), *pyplot_args, **pyplot_kwargs)
-    elif ((len(Modes)==2) and ((Modes[0]<2) or (Modes[0]<abs(Modes[1])))) :
-        Lines = styledplot(this.T(), transpose((quantity(Modes[0]),quantity(Modes[1]))), *pyplot_args, **pyplot_kwargs)
+        Labels = ['(' + str(this.L(mode)) + ', ' + str(this.M(mode)) + ')' for mode in range(this.NModes())]
+    elif ((len(Modes)==2) and (type(Modes[0])==int and type(Modes[1])==int) and ((Modes[0]<2) or (Modes[0]<abs(Modes[1])))) :
+        Lines = styledplot(this.T(), transpose((quantity(Modes[0]), quantity(Modes[1]))), *pyplot_args, **pyplot_kwargs)
+        Labels = ['(' + str(this.L(mode)) + ', ' + str(this.M(mode)) + ')' for mode in Modes]
     else :
         Modes = array(Modes)
         if (Modes.shape==(2,)) :
             warn("Ambiguous 'Modes' specification.  Assuming this is an (l,m) specification.", SyntaxWarning)
             #try: # Catch a bad (l,m) error
             Lines = styledplot(this.T(), quantity(this.FindModeIndex(int(Modes[0]), int(Modes[1]))), *pyplot_args, **pyplot_kwargs)
+            Labels = ['(' + str(Modes[0]) + ', ' + str(Modes[1]) + ')']
         else :
             Data = empty([Modes.shape[0], this.NTimes()], dtype=float)
             for i in range(Modes.shape[0]) :
                 if (type(Modes[i])==int) :
                     ModeIndex = Modes[i]
+                    Labels.append('(' + str(this.L(Modes[i])) + ', ' + str(this.M(Modes[i])) + ')')
                 else :
                     ModeIndex = this.FindModeIndex(int(Modes[i][0]), int(Modes[i][1]))
+                    Labels.append('(' + str(Modes[i][0]) + ', ' + str(Modes[i][1]) + ')')
                 Data[i] = quantity(ModeIndex)
                 Lines = styledplot(this.T(), Data.transpose(), *pyplot_args, **pyplot_kwargs)
     
@@ -124,7 +104,10 @@ def plot(this, WaveformPart='Mag', Modes=(), *pyplot_args, **pyplot_kwargs) :
     try :
         tight_layout(pad=0.1)
     except RuntimeError :
-        
+        pass
+    
+    for i in range(len(Lines)) :
+        setp(Lines[i], label=Labels[i])
     
     return Lines
 
@@ -132,6 +115,6 @@ def plot(this, WaveformPart='Mag', Modes=(), *pyplot_args, **pyplot_kwargs) :
 
 # The following allows us to write things like
 #   Waveform W
-#   W.plot()
+#   W.plot('Mag', ((2,2), (2,-2)))
 import PyGW
 PyGW.WaveformObjects.Waveform.plot = plot
