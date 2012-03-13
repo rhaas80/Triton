1c12d322fe4a3548169eb0db4ed3efd25adcdc28
Mike Boyle <boyle@astro.cornell.edu>
2012-03-13 00:35:54 -0400

    Adding history and two new constructors to Waveforms, as well as a new member function -- Merge

--- Objects/Waveforms.cpp
+++ Objects/Waveforms.cpp
@@ -2,7 +2,14 @@
 
 #include "Waveforms.hpp"
 
+#include <ctime>
+#include <unistd.h>
+#include <sys/param.h>
+
+//#include <fstream>
+
 #include "VectorFunctions.hpp"
+#include "EasyParser.hpp"
 #include "Fit.hpp"
 
 using namespace WaveformUtilities;
@@ -13,23 +20,65 @@ using std::setprecision;
 using std::cerr;
 using std::cout;
 using std::endl;
+using std::ios_base;
 
 
 // Constructors
 
-Waveforms::Waveforms(const Waveforms& In) :
-  Ws(In.Ws), CommonTimeSet(In.CommonTimeSet), PhasesAligned(In.PhasesAligned)
-{ }
-
-Waveforms::Waveforms(const int N) :
-  Ws(N), CommonTimeSet(false), PhasesAligned(false)
-{ }
-
-Waveforms::Waveforms(const std::vector<double>& Radii, const std::string& DataFile,
-		     const std::string& AreaFile, const std::string& LapseFile,
-		     const double& ADMMass, const double& ChMass, const bool ZeroEnds) :
-  Ws(Radii.size()), CommonTimeSet(false), PhasesAligned(false)
+/// Basic copy constructor.
+WaveformObjects::Waveforms::Waveforms(const Waveforms& In) :
+  history(In.history.str()), Ws(In.Ws), CommonTimeSet(In.CommonTimeSet), PhasesAligned(In.PhasesAligned)
 {
+  history.seekp(0, ios_base::end);
+}
+
+/// Nearly-empty constructor of N empty objects.
+WaveformObjects::Waveforms::Waveforms(const int N) :
+  history("### Waveforms(); // empty constructor\n"),
+  Ws(N), CommonTimeSet(false), PhasesAligned(false)
+{
+  history.seekp(0, ios_base::end);
+}
+
+/// Construct from a vector of radii and corresponding data file names.
+WaveformObjects::Waveforms::Waveforms(const std::vector<double>& Radii, const std::string& DataFile,
+				      const std::string& AreaFile, const std::string& LapseFile,
+				      const double& ADMMass, const double& ChMass, const bool ZeroEnds) :
+  history(""), Ws(Radii.size()), CommonTimeSet(false), PhasesAligned(false)
+{
+  /// This constructor is used for extrapolation, primarily.  The
+  /// various file names are expected to be printf-formatted strings,
+  /// where each radius is the quantity used in the printf statement.
+  /// Thus, for example, '(100, 110, ...)' and
+  /// 'rPsi4_R%04.0fm_U8+.dat' may be input, where the file names are
+  /// 'rPsi4_R0100m_U8+.dat', 'rPsi4_R0110m_U8+.dat', etc.  The input
+  /// AreaFile and LapseFile are treated similarly.
+  
+  // Record the construction of this object
+  {
+    char path[MAXPATHLEN];
+    getcwd(path, MAXPATHLEN);
+    string pwd = path;
+    char host[MAXHOSTNAMELEN];
+    gethostname(host, MAXHOSTNAMELEN);
+    string hostname = host;
+    time_t rawtime;
+    time ( &rawtime );
+    string date = asctime ( localtime ( &rawtime ) );
+    history << "### Code revision `git rev-parse HEAD` = " << GitRevision << endl
+	    << "### pwd = " << pwd << endl
+	    << "### hostname = " << hostname << endl
+	    << "### date = " << date // comes with a newline
+	    << "### Waveforms(" << RowFormat(Radii) << ", "
+	    << DataFile << ", "
+	    << AreaFile << ", "
+	    << LapseFile << ", "
+	    << ADMMass << ", "
+	    << ChMass << ", "
+	    << ZeroEnds << ");" << endl;
+  }
+  
+  // Read the data into the Waveform objects, and adjust the time appropriately
   int BufferSize = 5000;
   for(unsigned int i=0; i<Radii.size(); ++i) { // Loop over Radii
     char DataFileName[BufferSize], AreaFileName[BufferSize], LapseFileName[BufferSize];
@@ -48,16 +97,161 @@ Waveforms::Waveforms(const std::vector<double>& Radii, const std::string& DataFi
 }
 
 
+/// Construct Waveforms from multiple [*-data] .bbh sections.
+WaveformObjects::Waveforms::Waveforms(const std::string& BBHFileName,
+				      const WaveformUtilities::Matrix<int> LM) :
+  history(""), Ws(0), CommonTimeSet(false), PhasesAligned(false)
+{
+  
+  // Record the construction of this object
+  {
+    char path[MAXPATHLEN];
+    getcwd(path, MAXPATHLEN);
+    string pwd = path;
+    char host[MAXHOSTNAMELEN];
+    gethostname(host, MAXHOSTNAMELEN);
+    string hostname = host;
+    time_t rawtime;
+    time ( &rawtime );
+    string date = asctime ( localtime ( &rawtime ) );
+    history << "### Code revision `git rev-parse HEAD` = " << GitRevision << endl
+	    << "### pwd = " << pwd << endl
+	    << "### hostname = " << hostname << endl
+	    << "### date = " << date // comes with a newline
+	    << "### Waveforms(" << BBHFileName << ", " << RowFormat(LM) << ");" << endl;
+  }
+  
+  // Loop through the .bbh file getting the various DataSections
+  vector<vector<string> > BBHDataSections;
+  vector<string> Dirs;
+  
+  
+  // Construct the vector, and read data into each element
+  Ws = vector<Waveform>(BBHDataSections.size());
+  for(unsigned int i=0; i<Ws.size(); ++i) {
+    Ws[i] = Waveform(BBHDataSections[i], Dirs[i], LM);
+  }
+}
+
+/// Construct Waveforms from a single [*-data] .bbh section.
+WaveformObjects::Waveforms::Waveforms(const std::vector<std::string>& BBHDataSection,
+				      const std::string Dir,
+				      const WaveformUtilities::Matrix<int> LM) :
+  history(""), Ws(0), CommonTimeSet(false), PhasesAligned(false)
+{
+  /// The resulting set of Waveforms really represents just one
+  /// Waveform, but each mode can have different times.  The modes can
+  /// be re-assembled into a single Waveform by calling the Merge()
+  /// method.  That is probably the most useful format.  This
+  /// constructor and Merge() are called by the Waveform::Waveform
+  /// constructor having the same arguments as this function.
+  
+  // Record the construction of this object
+  {
+    char path[MAXPATHLEN];
+    getcwd(path, MAXPATHLEN);
+    string pwd = path;
+    char host[MAXHOSTNAMELEN];
+    gethostname(host, MAXHOSTNAMELEN);
+    strng hostname = host;
+    time_t rawtime;
+    time ( &rawtime );
+    string date = asctime ( localtime ( &rawtime ) );
+    history << "### Code revision `git rev-parse HEAD` = " << GitRevision << endl
+	    << "### pwd = " << pwd << endl
+	    << "### hostname = " << hostname << endl
+	    << "### date = " << date // comes with a newline
+	    << "### Waveforms(\"\"\"";
+    for(unsigned int i=0; i<BBHDataSection.size(); ++i) {
+      history << "###              " << BBHDataSection[i] << endl;
+    }
+    history << "###           \"\"\"," << endl
+	    << "###           " << Dir << ", " << RowFormat(LM) << ");" << endl;
+  }
+  
+  // Figure out the format and data type
+  unsigned int WaveformTypeIndex = 11;
+  std::vector<std::vector<int> > WaveformLMs(0, std::vector<int>(2));
+  std::vector<std::string> FileNames(0);
+  unsigned int i=0;
+  std::string Format;
+  while(i<BBHDataSection.size()) {
+    string TrimmedLine = TrimWhiteSpace(StripComments(BBHDataSection[i]));
+    if(TrimmedLine.compare("[ht-data]") == 0) {
+      WaveformTypeIndex = 11;
+      Format = "ReIm";
+      break;
+    } else if(TrimmedLine.compare("[psi4t-data]") == 0) {
+      WaveformTypeIndex = 9;
+      Format = "ReIm";
+      break;
+    } else if(TrimmedLine.compare("[ht-ampphi-data]") == 0) {
+      WaveformTypeIndex = 11;
+      Format = "MagArg";
+      break;
+    } else if(TrimmedLine.compare("[psi4t-ampphi-data]") == 0) {
+      WaveformTypeIndex = 9;
+      Format = "MagArg";
+      break;
+    }
+    ++i;
+  }
+  ++i;
+  
+  // Loop through following lines getting data
+  string DataFileName = "";
+  std::vector<std::vector<double> > Times(0);
+  std::vector<std::vector<double> > Re(0);
+  std::vector<std::vector<double> > Im(0);
+  std::vector<int> LandM(2);
+  std::vector<string> Pair(2,"");
+  std::vector<string> LandMString(2,"");
+  string Line="";
+  while(i<BBHDataSection.size()) {
+    // Parse this line
+    string LineCharsStripped = TrimWhiteSpace(StripComments(BBHDataSection[i]));
+    if(LineCharsStripped.length()==0) { ++i; continue; } // skip empty lines
+    if(LineCharsStripped.find("-data]")!=string::npos) { break; } // end if we've reached another [*-data] section
+    Pair = split(Line.assign(LineCharsStripped), '=');
+    if(Pair[0].find("extraction-radius")!=string::npos) { ++i; continue; } // ignore extraction-radius line
+    LandMString = split(Pair[0], ',');
+    DataFileName = TrimWhiteSpace(Pair[1]);
+    LandM[0] = atoi(LandMString[0].c_str());
+    LandM[1] = atoi(LandMString[1].c_str());
+    if(LM.nrows()==0) {
+      WaveformLMs.push_back(LandM);
+      FileNames.push_back(Dir+DataFileName);
+    } else {
+      for(unsigned int j=0; j<LM.nrows(); ++j) {
+	if(LandM[0] == LM[j][0] && LandM[1] == LM[j][1]) {
+	  WaveformLMs.push_back(LandM);
+	  FileNames.push_back(Dir+DataFileName);
+	  break;
+	}
+      }
+    }
+    ++i;
+  }
+  
+  Ws = std::vector<Waveform>(WaveformLMs.size());
+  for(unsigned int j=0; j<Ws.size(); ++j) {
+    Ws[j] = Waveform(FileNames[j], Format);
+    //Ws[j].TypeIndexRef() = WaveformTypeIndex;
+    Ws[j].LMRef(0) = WaveformLMs[j];
+  }
+}
+
 
 // Member functions
 
-void Waveforms::SetCommonTime(const double& MinStep, const double& MinTime) {
+void WaveformObjects::Waveforms::SetCommonTime(const double& MinStep, const double& MinTime) {
+  history << "### this->SetCommonTime(" << MinStep << ", " << MinTime << ");" << endl;
   std::vector<double> Time(Ws[0].T());
-  //// Get the intersection of all the time data
+  // Get the intersection of all the time data
   for(unsigned int i=1; i<Ws.size(); ++i) {
     Time = Intersection(Time, Ws[i].T(), MinStep, MinTime);
   }
-  //// Interpolate each Waveform to the common time series
+  // Interpolate each Waveform to the common time series
   for(unsigned int i=0; i<Ws.size(); ++i) {
     Ws[i].Interpolate(Time);
   }
@@ -65,15 +259,17 @@ void Waveforms::SetCommonTime(const double& MinStep, const double& MinTime) {
   return;
 }
 
-void Waveforms::FixNonOscillatingData() {
-  //// Step through each Waveform doing the work
+void WaveformObjects::Waveforms::FixNonOscillatingData() {
+  history << "### this->FixNonOscillatingData();" << endl;
+  // Step through each Waveform doing the work
   for(unsigned int i=0; i<Ws.size(); ++i) {
     Ws[i].FixNonOscillatingData();
   }
   return;
 }
 
-void Waveforms::AlignPhases(const double& AlignmentPoint) {
+void WaveformObjects::Waveforms::AlignPhases(const double& AlignmentPoint) {
+  history << "### this->AlignPhases(" << AlignmentPoint << ");" << endl;
   if(!CommonTimeSet) { SetCommonTime(); }
   int I = int(floor(AlignmentPoint * Ws[0].NTimes()));
   for(unsigned int i=1; i<Ws.size(); ++i) { // Loop over Waveforms
@@ -85,12 +281,16 @@ void Waveforms::AlignPhases(const double& AlignmentPoint) {
   return;
 }
 
-Waveform Waveforms::Extrapolate(const int ExtrapolationOrder) {
+Waveform WaveformObjects::Waveforms::Extrapolate(const int ExtrapolationOrder) {
+  history << "### this->Extrapolate(" << ExtrapolationOrder << ");" << endl;
+  
   if(!PhasesAligned) { AlignPhases(); }
   if(ExtrapolationOrder<0) { return Ws[Ws.size() + ExtrapolationOrder]; }
   Waveform Extrap = Ws[0];
+  Extrap.SetHistory(history.str());
+  Extrap.History() << "#### NOTE: This object is now a single Waveform (extrapolated from a 'Waveforms' object)." << endl;
   
-  //// Set up the important variables
+  // Set up the important variables
   vector<double> oor(Ws.size(), 0.0);  // one over radii
   vector<double> amp(Ws.size(), 0.0);
   vector<double> phi(Ws.size(), 0.0);
@@ -99,7 +299,7 @@ Waveform Waveforms::Extrapolate(const int ExtrapolationOrder) {
   Fit<PolynomialBasisFunctions> ampFit(oor, amp, sig, Poly);
   Fit<PolynomialBasisFunctions> phiFit(oor, phi, sig, Poly);
   
-  //// Loop over time
+  // Loop over time
   for(unsigned int i=0; i<Extrap.NTimes(); ++i) {
     if(i % 1000 == 0) {
       cout << "Time = " << setprecision(5) << Extrap.T(i) << "\tStep " << i << " of " << Extrap.NTimes() << endl;
@@ -129,5 +329,31 @@ Waveform Waveforms::Extrapolate(const int ExtrapolationOrder) {
   }
   Extrap.RRef() = vector<double>(1, numeric_limits<double>::infinity( ) );
   
+  Extrap.History() << "#### Extrapolation finished." << endl;
+  
   return Extrap;
+}
+
+Waveform WaveformObjects::Waveforms::Merge(const double& MinStep, const double& MinTime) {
+  history << "### this->Merge(" << MinStep << ", " << MinTime << ");" << endl;
+  
+  if(Ws.size()<2) {
+    cerr << "Ws.size()=" << Ws.size() << endl;
+    throw("Cant' Merge fewer than 2 Waveforms");
+  }
+  SetCommonTime(MinStep, MinTime);
+  Waveform Merged = Ws[0];
+  Merged.SetHistory(history.str());
+  Merged.History() << "#### NOTE: This object is now a single Waveform (merged from a 'Waveforms' object)." << endl;
+  Merged.LMRef().resize(Ws.size(), 2);
+  Merged.MagRef().resize(Ws.size(), Ws[0].NTimes());
+  Merged.ArgRef().resize(Ws.size(), Ws[0].NTimes());
+  for(unsigned int i=1; i<Ws.size(); ++i) {
+    Merged.LRef(i) = Ws[i].L(0);
+    Merged.MRef(i) = Ws[i].M(0);
+    Merged.MagRef(i) = Ws[i].Mag(0);
+    Merged.ArgRef(i) = Ws[i].Arg(0);
+  }
+  Merged.History() << "#### Merge finished." << endl;
+  return Merged;
 }
--- Objects/Waveforms.hpp
+++ Objects/Waveforms.hpp
@@ -3,9 +3,15 @@
 
 #include "Waveform.hpp"
 
-namespace WaveformObjects{
+namespace WaveformObjects {
   
   class Waveforms {
+    
+  private:  // Member data
+    std::stringstream history;
+    std::vector<Waveform> Ws;
+    bool CommonTimeSet;
+    bool PhasesAligned;
     
   public:  // Constructors and Destructor
     Waveforms(const Waveforms& In);
@@ -13,6 +19,11 @@ namespace WaveformObjects{
     Waveforms(const std::vector<double>& Radii, const std::string& DataFile,
 	      const std::string& AreaFile, const std::string& LapseFile,
 	      const double& ADMMass, const double& ChMass, const bool ZeroEnds=false);
+    Waveforms(const std::string& BBHFileName,
+	      const WaveformUtilities::Matrix<int> LM=WaveformUtilities::Matrix<int>(0,0));
+    Waveforms(const std::vector<std::string>& BBHDataSection,
+	      const std::string Dir = "",
+	      const WaveformUtilities::Matrix<int> LM=WaveformUtilities::Matrix<int>(0,0));
     ~Waveforms() { }
     
   public:  // Operators
@@ -24,12 +35,8 @@ namespace WaveformObjects{
     void FixNonOscillatingData();
     void AlignPhases(const double& AlignmentPoint=0.5);
     Waveform Extrapolate(const int ExtrapolationOrder=5);
+    Waveform Merge(const double& MinStep=0.0, const double& MinTime=0.0);
     void clear() { Ws.clear(); CommonTimeSet=false; PhasesAligned=false; }
-    
-  private:  // Member data
-    std::vector<Waveform> Ws;
-    bool CommonTimeSet;
-    bool PhasesAligned;
     
   }; // class
 
