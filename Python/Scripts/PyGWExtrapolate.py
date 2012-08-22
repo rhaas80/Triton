805f72edb923d43d7d618e293384b1399e99f393
Mike Boyle <boyle@astro.cornell.edu>
2012-08-22 17:07:19 -0400

    Add comments, and allow this to work with very old matplotlib

--- Python/Scripts/PyGWExtrapolate.py
+++ Python/Scripts/PyGWExtrapolate.py
@@ -3,11 +3,78 @@
 """
 Extrapolate a series of waveforms from a set of dat files.
 
+This file contains a class to do the extrapolation.  For details, see
+the help text of the Extrapolate class.
+
+Note that data in the new format (HDF5) need to be extrapolated with
+the separate script 'PyGWExtrapolate_h5.py'.
+
 """
 
 ## We use a class just to be able to set the parameters nicely.  This
 ## should be treated basically like a function.
 class Extrapolate :
+    """
+    Class used to set options and do extrapolation.
+    
+    Construct this object to read Waveform data, lapse, and surface
+    area data for a series of radii, and extrapolate to infinite
+    radius.  Note that data in the new format (HDF5) need to be
+    extrapolated with the separate script 'PyGWExtrapolate_h5.py'.
+    
+    Options	 	 Defaults
+    =================================================================
+    Radii	 	 []
+        List of strings containing the radii to use.  If none are
+        input, all data files that can be found are used.
+    
+    ExtrapolationOrders	 [-1, 2, 3, 4, 5, 6, 7, 8]
+        Negative numbers correspond to extracted data, counting 
+        down from the outermost extraction radius (which is -1).
+    
+    InputDirectory	 "./"
+        Where to find the input data.  Can be relative or absolute.
+    
+    OutputDirectory	 "./"
+        This directory will be made if it does not exist.
+    
+    DataFiles	 	 ["rh_R%04.0fm.dat", "rPsi4_R%04.0fm_U8+.dat"]
+    AreaFile	 	 "SurfaceArea_R%04.0fm.dat"
+    LapseFile	 	 "LapseSurfaceIntegral_R%04.0fm.dat"
+        These are sprintf-formatted file names, where each of Radii
+        are converted to doubles and substituted into these strings.
+        These are the input data required for extrapolation.
+    
+    ADMMass	 	 1.0
+    ChMass	 	 1.0
+        One or both of these masses should be set.  Both are
+        considered to be in the current units.
+    
+    ExtrapolatedFiles	 "ExtrapolatedN%d.dat"
+    DifferenceFiles	 "ExtrapConvergence_N%d-N%d.dat"
+    SigmaFiles	 	 "SigmaN%d.dat"
+        These are sprintf-formatted output file names, where the
+        extrapolation order is substituted into these strings.
+    
+    UseSVD	 	 True
+        Use SVD to do the fitting.  This should generally be true, and
+        is here to support comparisons with old results.
+    
+    UseOmegas	 	 True
+        Whether or not to extrapolate as a function of lambda/r =
+        1/r*omega, where lambda is the instantaneous wavelength.  If
+        this is True, the extrapolation will usually not converge for
+        high N; if this is False, SVD will generally cause the
+        convergence to appear to fall to roundoff, though the accuracy
+        presumably is not so great.
+    
+    PlotFormat 	 	 'png'
+        The format of output plots.  This can be the empty string, in
+        which case no plotting is done.  Or, these can be any of the
+        formats supported by your installation of matplotlib.
+
+    """
+    
     def SetParameter(self, VariableName, DefaultValue) :
         if VariableName in self.ParameterDictionary :
             setattr(self, VariableName, self.ParameterDictionary[VariableName])
@@ -16,12 +83,11 @@ class Extrapolate :
     
     def __init__ (self, FileName="", Dictionary={}) :
         import PyGW
-        import PyGW.plot
         import numpy as npy
-        import matplotlib.pyplot as plt
         from time import time
         import sys
         import os
+        
         self.ParameterDictionary=Dictionary
         
         # Set up the parameters, using default values or the value
@@ -40,10 +106,16 @@ class Extrapolate :
         self.SetParameter('SigmaFiles', "SigmaN%d.dat")
         self.SetParameter('UseSVD', True)
         self.SetParameter('UseOmegas', True)
+        self.SetParameter('PlotFormat', 'png')
         
         # If there's an input file, read it in
         if(FileName!="") :
             execfile(FileName)
+            
+        # Don't bother loading modules unless we're plotting
+        if(self.PlotFormat) :
+            import PyGW.plot
+            import matplotlib.pyplot as plt
         
         # For some compatibility with old input files
         try :
@@ -72,17 +144,16 @@ class Extrapolate :
                 raise ValueError("Not enough data sets ({0}) for min extrapolation order (N={1}).".format(len(self.Radii), min(self.ExtrapolationOrders)))
             # Read in the Waveforms and set things up nicely
             Ws = PyGW.Waveforms(npy.array([float(s) for s in self.Radii]),
-                                self.InputDirectory+DataFile, self.InputDirectory+self.AreaFile, self.InputDirectory+self.LapseFile,
+                                self.InputDirectory+'/'+DataFile, self.InputDirectory+'/'+self.AreaFile, self.InputDirectory+'/'+self.LapseFile,
                                 self.ADMMass, self.ChMass, False);
             Ws.SetCommonTime();
             Ws.FixNonOscillatingData();
             
             # Loop over all the ExtrapolationOrders, doing the dirty business
-            import PyGW.plot
-            import matplotlib.pyplot as plt
             Last = PyGW.Waveform()
-            figmag = plt.figure('Mag')
-            figarg = plt.figure('Arg')
+            if(self.PlotFormat) :
+                figmag = plt.figure(0)
+                figarg = plt.figure(1)
             WFType = ''
             for i in range(len(self.ExtrapolationOrders)) :
                 print("Extrapolating with order N={}.".format(self.ExtrapolationOrders[i]))
@@ -127,29 +198,32 @@ class Extrapolate :
                     sys.stdout.flush()
                     PyGW.Output(self.OutputDirectory+"/"+DifferenceFile, Diff)
                     print("☺")
-                    sys.stdout.write("Plotting... ")
-                    sys.stdout.flush()
-                    plt.figure('Mag')
-                    Diff.plot('LogMag', Modes=[[2,2]], label=r'$(N={0}) - (N={1})$'.format(self.ExtrapolationOrders[i], self.ExtrapolationOrders[i-1]))
-                    plt.figure('Arg')
-                    Diff.plot('LogArg', Modes=[[2,2]], label=r'$(N={0}) - (N={1})$'.format(self.ExtrapolationOrders[i], self.ExtrapolationOrders[i-1]))
-                    print("☺")
+                    if(self.PlotFormat) :
+                        sys.stdout.write("Plotting... ")
+                        sys.stdout.flush()
+                        plt.figure(0)
+                        Diff.plot('LogMag', Modes=[[2,2]], label=r'$(N={0}) - (N={1})$'.format(self.ExtrapolationOrders[i], self.ExtrapolationOrders[i-1]))
+                        plt.figure(1)
+                        Diff.plot('LogArg', Modes=[[2,2]], label=r'$(N={0}) - (N={1})$'.format(self.ExtrapolationOrders[i], self.ExtrapolationOrders[i-1]))
+                        print("☺")
                 
                 # Save this one for the convergence plots
                 Last.swap(Extrap);
             
-            plt.figure('Mag')
-            plt.legend(borderpad=.2, labelspacing=0.1, handlelength=1.5, handletextpad=0.1, loc='lower left', prop={'size':'small'})
-            plt.gca().set_ylim(1e-11, 10)
-            plt.gca().axvline(x=MaxFluxTime, ls='--')
-            figmag.savefig('{0}/ExtrapConvergence_Mag_{1}.pdf'.format(self.OutputDirectory, WFType))
-            plt.close(figmag)
-            plt.figure('Arg')
-            plt.legend(borderpad=.2, labelspacing=0.1, handlelength=1.5, handletextpad=0.1, loc='lower left', prop={'size':'small'})
-            plt.gca().set_ylim(1e-11, 10)
-            plt.gca().axvline(x=MaxFluxTime, ls='--')
-            figarg.savefig('{0}/ExtrapConvergence_Arg_{1}.pdf'.format(self.OutputDirectory, WFType))
-            plt.close(figarg)
+            # Output the plots
+            if(self.PlotFormat) :
+                plt.figure(0)
+                plt.legend(borderpad=.2, labelspacing=0.1, handlelength=1.5, handletextpad=0.1, loc='lower left', prop={'size':'small'})
+                plt.gca().set_ylim(1e-11, 10)
+                plt.gca().axvline(x=MaxFluxTime, ls='--')
+                figmag.savefig('{0}/ExtrapConvergence_Mag_{1}.{2}'.format(self.OutputDirectory, WFType, PlotFormat))
+                plt.close(figmag)
+                plt.figure(1)
+                plt.legend(borderpad=.2, labelspacing=0.1, handlelength=1.5, handletextpad=0.1, loc='lower left', prop={'size':'small'})
+                plt.gca().set_ylim(1e-11, 10)
+                plt.gca().axvline(x=MaxFluxTime, ls='--')
+                figarg.savefig('{0}/ExtrapConvergence_Arg_{1}.{2}'.format(self.OutputDirectory, WFType, PlotFormat))
+                plt.close(figarg)
 
 if __name__ == "__main__":
     import sys
