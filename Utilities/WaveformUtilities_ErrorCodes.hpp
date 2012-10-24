acf7ae4c556406b828294d91e6b7f1479d99eaef
Mike Boyle <boyle@astro.cornell.edu>
2012-10-24 16:26:38 -0400

    Central repository for error codes

---
+++ Utilities/WaveformUtilities_ErrorCodes.hpp
@@ -0,0 +1,15 @@
+#ifndef WAVEFORMUTILITIES_ERRORCODES_HPP
+#define WAVEFORMUTILITIES_ERRORS_HPP
+
+// Note: These error codes are used for python especially
+
+#define WaveformUtilities_IndexOutOfBounds 1
+#define WaveformUtilities_InfinitelyManySolutions 2
+#define WaveformUtilities_VectorSizeMismatch 3
+#define WaveformUtilities_CannotExtrapolateQuaternions 4
+#define WaveformUtilities_MatrixSizeMismatch 5
+#define WaveformUtilities_MatrixSizeAssumedToBeThree 6
+#define WaveformUtilities_QuaternionVectorSizeNotUnderstood 7
+#define WaveformUtilities_WaveformMissingLMIndex 8
+
+#endif // WAVEFORMUTILITIES_ERRORCODES_HPP
